7add1ea341ee9e7b1e9c4089e32fce321b1cd68e
cnking <cnking@umich.edu>
2022-04-16 13:25:59 -0400

    fixed header

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -42,20 +42,20 @@ export default function Projects({ data, content }) {
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <header className="text-right m-0 p-3 bg-white fixed top-0 left-0 right-0 z-10">
+        <header className="text-right m-0 p-3 bg-white sticky top-0 left-0 right-0 z-10">
             <Link href="/">
                 <a className="text-6xl font-bold hover:underline">Camden King</a>
             </Link>
         </header>
 
-        <main className='grid place-items-center p-4'>
+        <main className='grid place-items-center p-4 relative'>
             <div className="">
                 {data.live_link ? 
                     <a href={data.live_link} target="_blank" rel="noopener noreferrer">
-                        <h1 className="font-bold text-7xl mt-20 mb-9 hover:underline">{data.title}</h1>
+                        <h1 className="font-bold text-7xl mt-5 mb-9 hover:underline">{data.title}</h1>
                     </a>
                 :   
-                    <h1 className="font-bold text-7xl mt-20 mb-9">{data.title}</h1>
+                    <h1 className="font-bold text-7xl mt-5 mb-9">{data.title}</h1>
                 }
                 <div className="prose max-w-3xl">
                     <MDXRemote components={components} {...content} />
